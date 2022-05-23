e73ed6f812b45b2e180e2d9794a221d495c5fb60
GusDeMarco076 <gusdemarco@outlook.com>
2022-05-23 19:51:59 -0400

    Nueva version

--- js/jscript.js
+++ js/jscript.js
@@ -6,3 +6,16 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+
+function ValidateEmail() {
+
+  var email = document.getElementById('Email');
+  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+  if (!filter.test(email.value)) {
+    alert('La dirección de email ingresada es inválida, favor intente nuevamente...');
+    email.focus;
+    return false;
+  }
+}
